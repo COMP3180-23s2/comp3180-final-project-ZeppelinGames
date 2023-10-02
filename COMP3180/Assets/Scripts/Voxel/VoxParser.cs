5aac04114cf3837e4cbdf0e662af8d10ef41e022
marksabamq <31507417+ZeppelinGames@users.noreply.github.com>
2023-10-02 22:08:55 +1100

    Cleaned

--- COMP3180/Assets/Scripts/Voxel/VoxParser.cs
+++ COMP3180/Assets/Scripts/Voxel/VoxParser.cs
@@ -3,26 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct VoxelPoint
-{
-    public Vector3Int Position;
-    public int ColorIndex;
-
-    public Vector3 WorldPosition => (Vector3)this.Position * VoxelBuilder.VoxelSize;
-
-    public VoxelPoint(Vector3Int v, int cIn)
-    {
-        this.Position = v;
-        this.ColorIndex = cIn;
-    }
-
-    public VoxelPoint(int x, int y, int z, int cIn)
-    {
-        this.Position = new Vector3Int(x, y, z);
-        this.ColorIndex = cIn;
-    }
-}
-
 public static class VoxParser
 { 
     enum LoadState
---
+++ COMP3180/Assets/Scripts/Voxel/VoxelPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public struct VoxelPoint
+{
+    public Vector3Int Position;
+    public int ColorIndex;
+
+    public Vector3 WorldPosition => (Vector3)this.Position * VoxelBuilder.VoxelSize;
+
+    public VoxelPoint(Vector3Int v, int cIn)
+    {
+        this.Position = v;
+        this.ColorIndex = cIn;
+    }
+
+    public VoxelPoint(int x, int y, int z, int cIn)
+    {
+        this.Position = new Vector3Int(x, y, z);
+        this.ColorIndex = cIn;
+    }
+}
